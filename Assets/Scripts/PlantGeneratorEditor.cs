9c2d1e427a3afcdc0cee392c5f328a265e1d8dae
Louis <louis_foy@yahoo.co.uk>
2019-02-23 13:09:41 +0000

    Added basic shoot and petal generators!

---
+++ Assets/Scripts/PlantGeneratorEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CustomEditor(typeof(PlantGenerator))]
+public class PlantGeneratorEditor : Editor {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Renders any editable points in the plant generator
+    void OnSceneGUI()
+    {
+        PlantGenerator generator = target as PlantGenerator;
+
+        for (int i = 0; i < generator.editablePoints.Length; i++)
+        {
+            // Render each piont with a purple handle
+            Handles.color = new Color(1.0f, 0.0f, 1.0f, 0.1f);
+            generator.editablePoints[i] = Handles.DoPositionHandle(generator.editablePoints[i], Quaternion.identity);
+        }
+
+        generator.doRegenerate = !generator.doRegenerate;
+    }
+}
